ec0a7cdae3a9f8afee3addef0a245271a573c1c3
Maks <maks.vogtman@gmail.com>
2023-05-08 14:58:42 +0100

    Initial Commit

---
+++ Project20.1/GameScene.swift
@@ -0,0 +1,240 @@
+//
+//  GameScene.swift
+//  Project20.1
+//
+//  Created by Maks Vogtman on 30/01/2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    var gameTimer: Timer?
+    var fireworks = [SKNode]()
+    var scoreLabel: SKLabelNode!
+    var launchesLabel: SKLabelNode!
+    var gameOverLabel: SKLabelNode!
+    
+    let leftEdge = 22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    
+    var numberOfLaunches = 0 {
+        didSet {
+            launchesLabel.text = "Launches: \(numberOfLaunches)/10"
+        }
+    }
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        scoreLabel = SKLabelNode(fontNamed: "AvenirNextCondensed-Bold")
+        scoreLabel.position = CGPoint(x: 8, y: 8)
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.text = "Score: 0"
+        scoreLabel.color = .white
+        scoreLabel.fontSize = 50
+        addChild(scoreLabel)
+        
+        launchesLabel = SKLabelNode(fontNamed: "AvenirNextCondensed-Bold")
+        launchesLabel.position = CGPoint(x: 1016, y: 8)
+        launchesLabel.horizontalAlignmentMode = .right
+        launchesLabel.text = "Launches: 0/10"
+        launchesLabel.color = .white
+        launchesLabel.fontSize = 50
+        addChild(launchesLabel)
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
+    }
+    
+    
+    func createFirework(xMovement: CGFloat, x: Int, y: Int) {
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.colorBlendFactor = 1
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        switch Int.random(in: 0...2) {
+        case 0:
+            firework.color = .cyan
+        case 1:
+            firework.color = .green
+       default:
+            firework.color = .red
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: xMovement, y: 1000))
+        
+        let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: 200)
+        node.run(move)
+        
+        if let emitter = SKEmitterNode(fileNamed: "fuse") {
+            emitter.position = CGPoint(x: 0, y: -22)
+            node.addChild(emitter)
+        }
+        
+        fireworks.append(node)
+        addChild(node)
+    }
+    
+    
+    @objc func launchFireworks() {
+        guard numberOfLaunches < 10 else { return gameOver() }
+        
+        let movementAmount: CGFloat = 1800
+        
+        switch Int.random(in: 0...3) {
+        case 0:
+            createFirework(xMovement: 0, x: 512, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 - 200, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 - 100, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 + 100, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 + 200, y: bottomEdge)
+            
+        case 1:
+            createFirework(xMovement: 0, x: 512, y: bottomEdge)
+            createFirework(xMovement: -200, x: 512 - 200, y: bottomEdge)
+            createFirework(xMovement: -100, x: 512 - 100, y: bottomEdge)
+            createFirework(xMovement: 100, x: 512 + 100, y: bottomEdge)
+            createFirework(xMovement: 200, x: 512 + 200, y: bottomEdge)
+            
+        case 2:
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 400)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 300)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 200)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 100)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge)
+            
+        case 3:
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 400)
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 300)
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 200)
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 100)
+            createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge)
+            
+        default:
+            break
+        }
+        
+        numberOfLaunches += 1
+    }
+    
+    
+    func gameOver() {
+        gameOverLabel = SKLabelNode(fontNamed: "AvenirNextCondensed-Bold")
+        gameOverLabel.position = CGPoint(x: 512, y: 324)
+        gameOverLabel.text = "Game Over"
+        gameOverLabel.color = .white
+        gameOverLabel.fontSize = 120
+        gameOverLabel.verticalAlignmentMode = .center
+        addChild(gameOverLabel)
+        
+        gameTimer?.invalidate()
+    }
+    
+    
+    func checkTouches(_ touches: Set<UITouch>) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let nodesAtPoint = nodes(at: location)
+        
+        for case let node as SKSpriteNode in nodesAtPoint {
+            guard node.name == "firework" else { continue }
+            
+            for parent in fireworks {
+                guard let firework = parent.children.first as? SKSpriteNode else { continue }
+                
+                if firework.name == "selected" && firework.color != node.color {
+                    firework.name = "firework"
+                    firework.colorBlendFactor = 1
+                }
+            }
+            
+            node.name = "selected"
+            node.colorBlendFactor = 0
+        }
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        checkTouches(touches)
+    }
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        checkTouches(touches)
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        for (index, firework) in fireworks.enumerated().reversed() {
+            if firework.position.y > 900 {
+                fireworks.remove(at: index)
+                firework.removeFromParent()
+            }
+        }
+    }
+    
+    
+    func explode(firework: SKNode) {
+        if let emitter = SKEmitterNode(fileNamed: "explode") {
+            emitter.position = firework.position
+            addChild(emitter)
+            
+            let wait = SKAction.wait(forDuration: 4)
+            let remove = SKAction.run { emitter.removeFromParent() }
+            let sequence = SKAction.sequence([wait, remove])
+            
+            emitter.run(sequence)
+        }
+        
+        firework.removeFromParent()
+    }
+    
+    
+    func explodeFireworks() {
+        var numExploded = 0
+        
+        for (index, fireworkContainer) in fireworks.enumerated().reversed() {
+            guard let firework = fireworkContainer.children.first as? SKSpriteNode else { continue }
+            
+            if firework.name == "selected" {
+                explode(firework: fireworkContainer)
+                fireworks.remove(at: index)
+                numExploded += 1
+            }
+        }
+        
+        switch numExploded {
+        case 0:
+            break
+        case 1:
+            score += 200
+        case 2:
+            score += 500
+        case 3:
+            score += 1500
+        case 4:
+            score += 2500
+        default:
+            score += 4000
+        }
+    }
+}
